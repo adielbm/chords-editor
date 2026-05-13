993721782b47d6b0a31590900ca8d8143bc5b247
adielBm <adiel.bm5@gmail.com>
2026-05-13 14:01:28 +0300

    s

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Hosted at https://adielbm.github.io/chords-editor
 export default defineConfig({
+  base: '/chords-editor/',
   plugins: [react()],
 });
